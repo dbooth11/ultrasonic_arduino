8e36aac7369fdfb84b3cd8c2e3dec333646ab46d
dbooth11 <dbooth11@gmail.com>
2015-10-19 21:34:32 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -4,133 +4,101 @@
 #include <EEPROM.h>
 #include <stdint.h>
 #include "Maxbotix.h"
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
+static const int RXPin = 2, TXPin = 3;
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin. Not used
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// These are the pins used for the music maker shield
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     8      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
 
-int upState = 0;
-int downState = 0;
-int volume = 20;
+static const uint32_t GPSBaud = 4800;
 byte offsetValue;
 long inches, cms, actual;
+char altstring[10], offsetstring[10]; // create string arrays
 /** Declare functions **/
 void serialOut();
 char inchStr[4];
-int sensor_status = 0; //to to test ready state
+
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(-1, 8, 6, 3, 4);
+
+//SS for LCD
+SoftwareSerial LCD(7,8); // pin 7 = TX, pin 6 = RX (unused)
 //Set up Ultrasonic.
 Maxbotix rangeSensorAD(A0, Maxbotix::AN, Maxbotix::HRLV);
-// Pins for Sound card
-#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
-#define SHIELD_CS     7      // VS1053 chip select pin (output)
-#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
-#define CARDCS 4    
-#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
-
-Adafruit_VS1053_FilePlayer musicPlayer = 
-  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+// The TinyGPS++ object
+TinyGPSPlus gps;
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup(){
-  Serial.begin(19200);
+  LCD.begin(9600);
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
   SD.begin(CARDCS);    // initialise the SD card
-  musicPlayer.setVolume(volume,volume);
-  delay(500);
-  
-  pinMode(A3, INPUT_PULLUP); // sets analog pin for volume up 
-  pinMode(A2, INPUT_PULLUP); // sets analog pin for volume down 
-// initialise the music player
-  if (! musicPlayer.begin()) { // initialise the music player
-     Serial.println(F("Couldn't find sound board, do you have the right pins defined?"));
-     while (1);
-  }
-  Serial.println(F("Sound board found"));
-  
-  sensor_status = sensor_status + SD.begin(CARDCS);    // initialise the SD card
-  // Set volume for left, right channels. lower numbers == louder volume!
   musicPlayer.setVolume(20,20);
+  delay(500);
+
+// If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
+  // audio playing
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
   
   // Set Sensor Offset. Get initial height off the ground in inches and write to permanent memory.
   cms = rangeSensorAD.getRange();
   inches = rangeSensorAD.toInches(cms);
-  if(cms > 10){
-    sensor_status = ++sensor_status;
-    }
-  //Sensor reads 195 when pointed into the open air. Check existing offset. If less than 80 inches, write a new offset.
-  if(inches < 80){
-    EEPROM.write(1, inches);
-  } 
+  EEPROM.write(1, inches);
   // Now grab the value we just wrote and use it as the offset in height calcs
   offsetValue = EEPROM.read(1);
-  //Ready to go. Let them know it's working.
-  if(sensor_status = 2){
-   musicPlayer.playFullFile("ready.mp3");
-   Serial.print("ready");
-   Serial.print(sensor_status);
-  }
+  LCD.print(254); // move cursor to beginning of first line
+  LCD.print(128);
+  LCD.print("         "); // clear display + legends
+  LCD.print("         ");
+  LCD.print("ACT INS "); // clear display + legends
+  LCD.print("OFFSET  ");
+
 }
 
-
+//After setup, this function just loops over and over.
 void loop() {
-  upState = digitalRead(A3);
-  downState = digitalRead(A2);
-  
-  adjustVolume();
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
   /** Main ultrasonic ping function and then convert to actual distance **/
   cms = rangeSensorAD.getRange();
   inches = rangeSensorAD.toInches(cms);
   actual = inches - offsetValue;
   
-Serial.println(upState);
-Serial.println(downState);
+
  if((actual <= 246) && (actual > 234)) {
-    musicPlayer.playFullFile("20feet.wav");
+    musicPlayer.playFullFile("2feet.wav");
     }
   else if((actual <= 126) && (actual > 114)) {
-  musicPlayer.playFullFile("10feet.wav");
+  musicPlayer.playFullFile("2feet.wav");
     }
-  else if((actual <= 66) && (actual > 54)) {
-   musicPlayer.playFullFile("5feet.wav");
+  else if((actual <= 86) && (actual > 54)) {
+   musicPlayer.playFullFile("2feet.wav");
     }
   else if((actual <= 42) && (actual > 30)) {
-   musicPlayer.playFullFile("3feet.wav");
+   musicPlayer.playFullFile("2feet.wav");
     }
   else if((actual <= 30) && (actual > 18)) {
    musicPlayer.playFullFile("2feet.wav");
     }
   else if((actual <= 18) && (actual > 6)) {
-   musicPlayer.playFullFile("hold.wav");
+ musicPlayer.playFullFile("2feet.wav");
     }
+  //sendtoLCD();
   serialOut();
-  delay(500);
+  delay(1000);
 } //loop
 
-void adjustVolume(){
-  //volume down
-  if(downState == 0){
-     volume = volume + 4;
-     musicPlayer.setVolume(volume,volume);
-     Serial.print("Volume Down");
-     Serial.println(volume);
-    }
-    //Volume up
-    if(upState == 0){
-       volume = volume - 4;
-       musicPlayer.setVolume(volume,volume);
-       Serial.print ("Volume Up");
-       Serial.println(volume);
-    }
-  
-  }
-  
-   // Write out data to serial monitor
-void serialOut() {
-    Serial.print("Raw height: ");
-    Serial.print(inches);
-    Serial.print("   ");
-  
-    Serial.print("Offset Value: ");
-    Serial.print(offsetValue);
-    Serial.print("   ");
-//Take inches value and subtract the offset to get height over ground.
-    Serial.print("Calc height: ");
-    Serial.print(inches - offsetValue);
-    Serial.println();
-}
